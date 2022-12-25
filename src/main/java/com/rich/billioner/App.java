134b7aa2aede12802cb81956102a83e8e3ce6c49
root <root@localhost.localdomain>
2022-12-25 20:33:38 +0530

    "This is first Commit"

---
+++ src/main/java/com/rich/billioner/App.java
@@ -0,0 +1,13 @@
+package com.rich.billioner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello billoner" );
+    }
+}
